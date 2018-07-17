a44e2acce17b499dbafef176527bf521ab7c5152
bennylibblibb <40160611+bennylibblibb@users.noreply.github.com>
2018-07-17 17:37:23 +0800

    Add project files.

---
+++ CentaSMSServer/CentaSMSServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CentaSMSServer
+{
+    public partial class CentaSMSServer : Form
+    {
+        public CentaSMSServer()
+        {
+            InitializeComponent();
+        }
+
+        private void CentaSMSServer_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CentaSMSServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace CentaSMSServer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string name = Directory.GetCurrentDirectory().Replace(@"\", "");
+            Mutex mutex = new Mutex(false, name);
+            bool Running = !mutex.WaitOne(0, false);
+            if (!Running)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new CentaSMSServer());
+            }
+            else
+            {
+                MessageBox.Show("This Application is Already Running！");
+            }
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new CentaSMSServer());
+        }
+    }
+}
